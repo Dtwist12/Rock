bcff75835bb3116182e7d26be42a7c20891d68a1
Joy Gogna <dasani407@gmail.com>
2020-10-01 18:46:40 -0400

    test

--- loop project/Loop.js
+++ loop project/Loop.js
@@ -1,7 +1,32 @@
-for(let i = 0; i < 1000 ; i++){
-    if(i % 2=== 0 && i % 1===0){
-       console.log(i)
-    }
-     
+for(let i = 1000; i > 0; i-=2){
+    
+       console.log(i);
 }
-   
+
+for( let i= 0; i< 10000; i++) {
+ if (i=2500){
+     alert('Quarter of the way there')
+
+     }   if (i=5000) {
+     alert('Halfway there')
+} 
+        if (i=10000) {
+            alert('The loop is done')
+             }
+            }
+
+
+var Favoriteshows = ["Golden Girls", "Madea", "Jimmy neutron", "Looney Tunes", "Tom & Jerry"];
+
+for(var i = 0; i < Favoriteshows.length; i++) 
+  if(Favoriteshows[i] === "Golden Girls") {
+      console.log("My Favorite #1 Tv show is " + Favoriteshows[0]);
+  } else if (Favoriteshows[i] === "Madea") {
+      console.log("My Favorite #2 Tv show is " + Favoriteshows[1]);
+  } else if (Favoriteshows[i] === "Jimmy neutron"){
+      console.log("My Favorite #3 Tv show is " + Favoriteshows[2]);
+  } else if(Favoriteshows[i] === "Looney Tunes"){
+      console.log("My Favorite #4 Tv show is " + Favoriteshows[3]);
+  } else if(Favoriteshows[i] === "Tom & Jerry"){
+      console.log("My Favorite #5 Tv show is " + Favoriteshows[4]);
+  }
