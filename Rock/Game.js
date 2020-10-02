29c395f38eb8280d0ba2c8ce6f3fb4f6239bc774
Joy Gogna <dasani407@gmail.com>
2020-10-02 09:18:38 -0400

    First commit

---
+++ Rock/Game.js
@@ -0,0 +1,15 @@
+var number = prompt("Enter number of rounds","10");
+
+if (number != 10) {document.getElementById("demo").innerHTML =
+ "you are playing" + number + "! games";
+}
+var a = parseInt("1") + "<br>";
+var b = parseInt("2") + "<br>";
+var c = parseInt("3") + "<br>";
+var d = parseInt("4") + "<br>";
+var e = parseInt("5") + "<br>";
+var f = parseInt("6") + "<br>";
+var g = parseInt("7") + "<br>";
+var h = parseInt("8") + "<br>";
+var i = parseInt("9") + "<br>";
+var j = parseInt("10") + "<br>"; 
