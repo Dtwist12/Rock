1a2bfe89aed4f0a7bb4a5be8884bd2e56c11f127
Joy Gogna <70154253+Dtwist12@users.noreply.github.com>
2020-10-04 23:55:53 -0400

    Add files via upload
    final

--- Rock/Game.js
+++ Rock/Game.js
@@ -1,15 +1,70 @@
-var number = prompt("Enter number of rounds","10");
 
-if (number != 10) {document.getElementById("demo").innerHTML =
- "you are playing" + number + "! games";
+
+
+
+var number = prompt("Enter number of rounds max 3")
+for(var i=0; i<3;i++){
+var parsedNumber = parseInt(3);
+
+
+
+ var letter = prompt('Enter r, p, or s');
+
+
+var Player;
+if(Player === "rock")
+ 
+(Player === "paper")
+ 
+(Player === "scissors")
+
+
+
+
+
+let computerNumbers = Math.floor(Math.random*3)
+
+
+var computer= computerNumbers ;
+if(computer === 2){
+    computer === "scissors"
+   }   else if(computer === 1){
+        computer === "paper"
+    } else if(computer == 0){
+        computer === "rock"
+    }
+
+for(i=0;i<1;i++){
+var computerwon;
+if (computer === "rock" && Player === "scissors"){
+    alert("Loser")
+
+}else if(computer === "scissors" && Player === "paper"){
+    alert("Loser")
+}else if(computer === "paper" && Player === "rock"){
+    alert("Loser")
 }
-var a = parseInt("1") + "<br>";
-var b = parseInt("2") + "<br>";
-var c = parseInt("3") + "<br>";
-var d = parseInt("4") + "<br>";
-var e = parseInt("5") + "<br>";
-var f = parseInt("6") + "<br>";
-var g = parseInt("7") + "<br>";
-var h = parseInt("8") + "<br>";
-var i = parseInt("9") + "<br>";
-var j = parseInt("10") + "<br>"; 
+
+
+var ties;
+if(Player === computer){
+    alert("Tie")
+}
+
+
+
+var Playerwon;
+if(Player === "rock" && computer === "scissors"){
+    alert("Winner!")
+
+}else if( Player === "scissors" && computer === "paper"){
+    alert("Winner!")
+
+}else if(Player === "paper" && computer === "rock"){
+    alert("Winner!")
+}
+}
+
+alert("You Have won " +Playerwon+ " times");
+alert("You Have lost " +computerwon+ " times");
+alert("You Have Tied " +ties+ " times")}
