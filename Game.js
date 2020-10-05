1adc58b962b233f7d77a76616e7be2380862d08a
Joy Gogna <70154253+Dtwist12@users.noreply.github.com>
2020-10-04 23:54:46 -0400

    Add files via upload
    final commit

---
+++ Game.js
@@ -0,0 +1,70 @@
+
+
+
+
+var number = prompt("Enter number of rounds max 3")
+for(var i=0; i<3;i++){
+var parsedNumber = parseInt(3);
+
+
+
+ var letter = prompt('Enter r, p, or s');
+
+
+var Player;
+if(Player === "rock")
+ 
+(Player === "paper")
+ 
+(Player === "scissors")
+
+
+
+
+
+let computerNumbers = Math.floor(Math.random*3)
+
+
+var computer= computerNumbers ;
+if(computer === 2){
+    computer === "scissors"
+   }   else if(computer === 1){
+        computer === "paper"
+    } else if(computer == 0){
+        computer === "rock"
+    }
+
+for(i=0;i<1;i++){
+var computerwon;
+if (computer === "rock" && Player === "scissors"){
+    alert("Loser")
+
+}else if(computer === "scissors" && Player === "paper"){
+    alert("Loser")
+}else if(computer === "paper" && Player === "rock"){
+    alert("Loser")
+}
+
+
+var ties;
+if(Player === computer){
+    alert("Tie")
+}
+
+
+
+var Playerwon;
+if(Player === "rock" && computer === "scissors"){
+    alert("Winner!")
+
+}else if( Player === "scissors" && computer === "paper"){
+    alert("Winner!")
+
+}else if(Player === "paper" && computer === "rock"){
+    alert("Winner!")
+}
+}
+
+alert("You Have won " +Playerwon+ " times");
+alert("You Have lost " +computerwon+ " times");
+alert("You Have Tied " +ties+ " times")}
